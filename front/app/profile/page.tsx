518fac8d6a4bfda4adb562b3e392b9379673a7fb
K-Ryo-ta <studyprograming51@gmail.com>
2024-03-08 20:22:49 +0900

    feat:graph create

--- front/app/profile/page.tsx
+++ front/app/profile/page.tsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { Payment, columns } from "../payments/columns"
+import { DataTable } from "../payments/data-tables"
 
-const page = () => {
+async function getData(): Promise<Payment[]> {
+    // Fetch data from your API here.
+    return [
+        {
+            id: "728ed52f",
+            title: "Payment 1",
+            fit: 1,
+            creativity: 1,
+            comprehensibility: 1,
+            moved: 1,
+            editing: 1,
+        },
+        {
+            id: "738ed52f",
+            title: "Payment 2",
+            fit: 1,
+            creativity: 1,
+            comprehensibility: 1,
+            moved: 1,
+            editing: 1,
+        },
+        {
+            id: "748ed52f",
+            title: "Payment 3",
+            fit: 1,
+            creativity: 1,
+            comprehensibility: 1,
+            moved: 1,
+            editing: 1,
+        },
+        {
+            id: "758ed52f",
+            title: "Payment 4",
+            fit: 1,
+            creativity: 1,
+            comprehensibility: 1,
+            moved: 1,
+            editing: 1,
+        },
+    ]
+}
+
+async function page() {
+    const data = await getData()
+
     return (
-        <div>page</div>
+        <div className="container mx-auto py-10">
+            <DataTable columns={columns} data={data} />
+
+        </div>
     )
 }
 
