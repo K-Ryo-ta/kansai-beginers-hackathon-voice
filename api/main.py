52f80ef060f89b9a086c443f39a14e102d7718ca
Nseigou <nagano_1si@yahoo.co.jp>
2024-03-10 00:07:43 +0900

    back OK

--- api/main.py
+++ api/main.py
@@ -1,103 +1,151 @@
 from typing import Union
 import os
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile,Response, Depends  
+from fastapi.responses import FileResponse
 from prisma import Prisma
 # 新しく追加したライブラリ。requirement.txtに追加する必要あり
 from datetime import datetime, timedelta
-import uuid
-from pydantic import Basemodel
+from pydantic import BaseModel
+from uuid import UUID, uuid4
+from fastapi_sessions.backends.implementations import InMemoryBackend
+from fastapi_sessions.session_verifier import SessionVerifier
+from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
+
+class Theme(BaseModel):
+    title: str
+    description: str
+    startDate: datetime
+    endDate: datetime
+
+class VideoDescription(BaseModel):
+    title: str
+    description: str
+    url: str   
+    thumbnail: str
+    theme_id: str
+    user_id: str
+
+class Login(BaseModel):
+    email: str
+    password: str
+
+class User(BaseModel):
+    email: str
+    password: str
+    name: str
+
+@app.post("/resister")
+async def create_user(user: User):
     db = Prisma()
     await db.connect()
-
-# ランダムなメールアドレスを生成する
-    random_email = f"{uuid.uuid4()}@example.com"
     user = await db.user.create(
         data={
-            'email': random_email,  # ランダムなメールアドレスを使用
-            'password': '12345',
-            'name': 'ryota',
+            'email': user.email,
+            "password":  user.password,
+            "name":       user.name,
         }
     )
+    await db.disconnect()
+    return {f"Hello{user.name}"}
 
-    start_date = datetime.now()
-    end_date = start_date + timedelta(days=7)
+
+
+@app.post("/theme/send")
+async def create_send_theme(theme: Theme):
+    db = Prisma()
+    await db.connect()
     theme = await db.theme.create(
         data={
-            'title': 'konitiwa',
-            'description': 'konitiwa',
-            'startDate': start_date,
-            'endDate': end_date,
+            'title': theme.title,
+            'description': theme.description,
+            'startDate': theme.startDate,
+            'endDate': theme.endDate
         }
     )
+    await db.disconnect()
+    return theme
 
+@app.get("/theme/{theme_id}")
+async def get_theme(theme_id: str):
+    db = Prisma()
+    await db.connect()
+    theme = await db.theme.find_first(where={"id": theme_id})
+    await db.disconnect()
+    return theme
+
+
+# サムネイルを保存する
+@app.post("/thumbnail/upload")
+async def create_upload_file(input: UploadFile):
+    os.makedirs("thumbnail", exist_ok=True)
+    with open(f"thumbnail/{input.filename}", "wb") as buffer:
+        buffer.write(input.file.read())
+    return {"filename": input.filename}
+
+# 動画を保存する
+@app.post("/video/upload")
+async def create_upload_file(input: UploadFile):
+    os.makedirs("uploads", exist_ok=True)
+    with open(f"uploads/{input.filename}", "wb") as buffer:
+        buffer.write(input.file.read())
+    return {"filename": input.filename}
+
+
+
+# 動画の説明やタイトルをほぞんする
+@app.post("/video/description")
+async def create_video_description(input: VideoDescription):
+    db = Prisma()
+    await db.connect()
     video = await db.video.create(
-        data={
-            'title': 'konitiwa',
-            'description': 'konitiwa',
-            'url': 'konitiwa',
-            'thumbnail': 'konitiwa',
+        data = {
+            'title': input.title,
+            'description': input.description,
+            'url': input.url,
+            'thumbnail': input.thumbnail,
             'user': {
-                'connect': {'id': user.id}
+                'connect': {'id': input.user_id}
             },
             'theme': {
-                'connect': {'id': theme.id}
+                'connect': {'id': input.theme_id}
             },
-        }
-    )
-
-    
-
-    evaluation = await db.evaluation.create(
-        data={
-            'user': {
-                'connect': {'id': user.id}
-            },
-            'video': {
-                'connect': {'id': video.id}
-            },
-            'fit': 1,
-            'creativity': 1,
-            'comprehensibility': 1,
-            'moved': 1,
-            'editing': 1,
-        }
-    )
-
-    userfound = await db.user.find_many(
-        include={
-            "videos": True
-        }
-    )
-    videofound = await db.video.find_many(
-        include={
-            'user': True,
-            'theme': True,
-            'evaluations': True,
-        }
-    )
-    themefound = await db.theme.find_many(
-        include={
-            'videos': True,
-        }
-    )
-    evaluationfound = await db.evaluation.find_many(
-        include={
-            'user': True,
-            'video': True,
-        }
-    )
-
-
+        })
     await db.disconnect()
-    return {
-    "Users": userfound, 
-    "Videos": videofound, 
-    "Themes": themefound, 
-    "Evaluations": evaluationfound,
-    # "QQQ":"aaa"
-}
+    return 
+    
+# 動画のせつめいをとる
+@app.get("/video/description/{id}")
+async def get_video_description(id:str):
+    db = Prisma()
+    await db.connect()
+    video = await db.video.find_first(where={"id": id})
+    await db.disconnect()
+    return 
+    
+#動画を表示する
+@app.get("/video/{filename}")
+async def get_video(filename:str):
+    return FileResponse(f"uploads/{filename}")
+
+#動画を削除する
+@app.delete("/video/{filename}")
+async def delete_video(filename:str):
+    #指定されたファイルを消す
+    os.remove("/upload/{filename}")
+    return {"message": f"{filename} deleted"}
+
+
+@app.get("/login")
+async def login(login: Login):
+    db = Prisma()
+    await db.connect()
+    user = await db.user.find_unique(where={"email" == input.email})
+    correct_password = user.password
+
+    if input.password == correct_password:
+        return "OK"
+    else:
+        return "fail"
